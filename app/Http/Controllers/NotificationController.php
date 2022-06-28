80e55df6af95522b521aea822a981ddb3ab6dcad
KIMCHANHO <kaelch@peoplenjob.com>
2022-06-28 15:39:57 +0900

    notification

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Notifications\OffersNotification;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
+
+class NotificationController extends Controller
+{
+    public function __construct()
+    {
+//        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('product');
+    }
+
+    public function sendOfferNotification() {
+        $userSchema = User::first();
+
+        $offerData = [
+            'name' => 'BOGO',
+            'body' => 'You received an offer.',
+            'thanks' => 'Thank you',
+            'offerText' => 'Check out the offer',
+            'offerUrl' => url('/'),
+            'offer_id' => 007
+        ];
+
+        Notification::send($userSchema, new OffersNotification($offerData));
+
+        dd('Task completed!');
+    }
+}
